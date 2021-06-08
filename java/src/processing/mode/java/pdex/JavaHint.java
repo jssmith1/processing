49f944b5e06d6e59906aa7b18ed759fcf7ea4dd2
soir20 <71418127+soir20@users.noreply.github.com>
2021-06-07 20:48:54 -0400

    Add dynamic titles for type mismatches

--- java/src/processing/mode/java/pdex/JavaHint.java
+++ java/src/processing/mode/java/pdex/JavaHint.java
@@ -6,6 +6,8 @@ import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import processing.app.ui.EditorHints;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class JavaHint implements EditorHints.Hint {
@@ -15,8 +17,6 @@ public class JavaHint implements EditorHints.Hint {
     private final List<String> BAD_CODE;
 
     public static List<EditorHints.Hint> fromIProblem(IProblem compilerError, ASTNode ast) {
-        List<EditorHints.Hint> hints = new ArrayList<>();
-
         String[] problemArguments = compilerError.getArguments();
         ASTNode problemNode = ASTUtils.getASTNodeAt(
                 ast,
@@ -25,27 +25,76 @@ public class JavaHint implements EditorHints.Hint {
         );
 
         if (compilerError.getID() == IProblem.TypeMismatch) {
-            String varName = ((VariableDeclarationFragment) problemNode.getParent()).getName().toString();
-            String providedType = problemArguments[0];
-            String requiredType = problemArguments[1];
-
-            String problemTitle = "You are trying to use the variable " + varName + " of type "
-                    + requiredType + " as a " + providedType + "-type variable.";
-
-            JavaHint hint = new JavaHint(problemTitle,
-                    "You may have assigned a " + providedType + " value to variable " + varName +
-                            " of type " + requiredType + "."
-            );
-
-            // TODO: provide different code samples based on provided type
-            // For number literals, the provided value is available after casting the node to a NumberLiteral
-            hint.addBadCode(requiredType + " " + varName + " = 5.0;");
-            hint.addGoodCode(providedType + " " + varName + " = 5.0;");
-
-            hints.add(hint);
+            String providedType = truncateClass(problemArguments[0]);
+            String requiredType = truncateClass(problemArguments[1]);
+            return getTypeMismatchHints(providedType, requiredType, problemNode);
         }
 
+        return Collections.emptyList();
+    }
+
+    private static List<EditorHints.Hint> getTypeMismatchHints(String providedType, String requiredType,
+                                                               ASTNode problemNode) {
+        List<EditorHints.Hint> hints = new ArrayList<>();
+
+        String varName = ((VariableDeclarationFragment) problemNode.getParent()).getName().toString();
+        String problemTitle = "You are trying to use the " + getVarDescription(requiredType)
+                + " " + varName + " as a " + getVarDescription(providedType) + ".";
+
+        JavaHint changeVarDec = new JavaHint(problemTitle,
+                "You might need to change the variable declaration of "
+                        + varName + " to type " + providedType + "."
+        );
+        hints.add(changeVarDec);
+
+        JavaHint changeValue = new JavaHint(problemTitle,
+                "You might need to change the value of "
+                        + varName + " to a " + requiredType + "."
+        );
+        hints.add(changeValue);
+
+        JavaHint changeReturnType = new JavaHint(problemTitle,
+                "You might need to change the method's return type to "
+                       + providedType + "."
+        );
+        hints.add(changeReturnType);
+
+        // Clarify numerical expressions where a float result is assigned to an int
+        if (providedType.equals("float") && requiredType.equals("int")) {
+            JavaHint changeOpType = new JavaHint(problemTitle,
+                    "You may have used an int-type variable " + varName
+                            + " in an operation involving the float type."
+            );
+            hints.add(changeOpType);
+        }
+
+        // TODO: provide different code samples based on provided type
+        // For number literals, the provided value is available after casting the node to a NumberLiteral
+        //hint.addBadCode(requiredType + " " + varName + " = 5.0;");
+        //hint.addGoodCode(providedType + " " + varName + " = 5.0;");
+
         return hints;
+    }
+
+    private static String getVarDescription(String typeName) {
+        List<String> primitives = Arrays.asList("byte", "short", "int", "long",
+                "float", "double", "boolean", "char");
+
+        if (primitives.contains(typeName)) {
+            return typeName + "-type variable";
+        }
+
+        return typeName + " object";
+    }
+
+    private static String truncateClass(String qualifiedName) {
+        int lastPeriodIndex = qualifiedName.lastIndexOf('.');
+
+        if (lastPeriodIndex == -1) {
+            return qualifiedName;
+        }
+
+        return qualifiedName.substring(lastPeriodIndex + 1);
     }
 
     public JavaHint(String problemText, String suggestionText) {
