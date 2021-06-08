38910bd3993b20409cbd79e0c027106addbd8449
soir20 <71418127+soir20@users.noreply.github.com>
2021-06-07 21:36:28 -0400

    Add code examples for type mismatches

--- java/src/processing/mode/java/pdex/JavaHint.java
+++ java/src/processing/mode/java/pdex/JavaHint.java
@@ -11,10 +11,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class JavaHint implements EditorHints.Hint {
-    private final String PROBLEM_TEXT;
-    private final String SUGGESTION_TEXT;
-    private final List<String> GOOD_CODE;
-    private final List<String> BAD_CODE;
+    private static final List<String> PRIMITIVES = Arrays.asList(
+            "byte", "short", "int", "long",
+            "float", "double", "boolean", "char"
+    );
 
     public static List<EditorHints.Hint> fromIProblem(IProblem compilerError, ASTNode ast) {
         String[] problemArguments = compilerError.getArguments();
@@ -24,10 +24,11 @@ public class JavaHint implements EditorHints.Hint {
                 compilerError.getSourceEnd()
         );
 
-        if (compilerError.getID() == IProblem.TypeMismatch) {
-            String providedType = truncateClass(problemArguments[0]);
-            String requiredType = truncateClass(problemArguments[1]);
-            return getTypeMismatchHints(providedType, requiredType, problemNode);
+        switch (compilerError.getID()) {
+            case IProblem.TypeMismatch:
+                String providedType = truncateClass(problemArguments[0]);
+                String requiredType = truncateClass(problemArguments[1]);
+                return getTypeMismatchHints(providedType, requiredType, problemNode);
         }
 
         return Collections.emptyList();
@@ -45,18 +46,30 @@ public class JavaHint implements EditorHints.Hint {
                 "You might need to change the variable declaration of "
                         + varName + " to type " + providedType + "."
         );
+        changeVarDec.addBadCode(getDemoDeclaration(requiredType, varName, providedType));
+        changeVarDec.addGoodCode(getDemoDeclaration(providedType, varName));
         hints.add(changeVarDec);
 
         JavaHint changeValue = new JavaHint(problemTitle,
                 "You might need to change the value of "
                         + varName + " to a " + requiredType + "."
         );
+        changeValue.addBadCode(getDemoDeclaration(requiredType, varName, providedType));
+        changeValue.addGoodCode(getDemoDeclaration(requiredType, varName));
         hints.add(changeValue);
 
         JavaHint changeReturnType = new JavaHint(problemTitle,
                 "You might need to change the method's return type to "
                        + providedType + "."
         );
+        changeReturnType.addBadCode(requiredType + " doSomething() {\n"
+                + "\t" + getDemoDeclaration(providedType, varName) + "\n"
+                + "\t" + "return " + varName + ";\n"
+                + "}");
+        changeReturnType.addGoodCode(providedType + " doSomething() {\n"
+                + "\t" + getDemoDeclaration(providedType, varName) + "\n"
+                + "\t" + "return " + varName + ";\n"
+                + "}");
         hints.add(changeReturnType);
 
         // Clarify numerical expressions where a float result is assigned to an int
@@ -65,26 +78,51 @@ public class JavaHint implements EditorHints.Hint {
                     "You may have used an int-type variable " + varName
                             + " in an operation involving the float type."
             );
+            changeOpType.addBadCode(getDemoDeclaration(requiredType, varName) + "\n"
+                    + varName + " = " + varName + " + 3.14;");
+            changeOpType.addGoodCode(getDemoDeclaration(providedType, varName) + "\n"
+                    + varName + " = " + varName + " + 3.14;");
             hints.add(changeOpType);
         }
-
-        // TODO: provide different code samples based on provided type
-        // For number literals, the provided value is available after casting the node to a NumberLiteral
-        //hint.addBadCode(requiredType + " " + varName + " = 5.0;");
-        //hint.addGoodCode(providedType + " " + varName + " = 5.0;");
 
         return hints;
     }
 
     private static String getVarDescription(String typeName) {
-        List<String> primitives = Arrays.asList("byte", "short", "int", "long",
-                "float", "double", "boolean", "char");
-
-        if (primitives.contains(typeName)) {
+        if (PRIMITIVES.contains(typeName) || typeName.equals("String")) {
             return typeName + "-type variable";
         }
 
         return typeName + " object";
+    }
+
+    private static String getDemoDeclaration(String decType, String varName) {
+        return getDemoDeclaration(decType, varName, decType);
+    }
+
+    private static String getDemoDeclaration(String decType, String varName, String valType) {
+        return decType + " " + varName + " = " + getDemoValue(valType) + ";";
+    }
+
+    private static String getDemoValue(String typeName) {
+        switch (typeName) {
+            case "byte":
+            case "short":
+            case "int":
+            case "long":
+                return "5";
+            case "float":
+            case "double":
+                return "5.0";
+            case "boolean":
+                return "true";
+            case "char":
+                return "'a'";
+            case "String":
+                return "\"hello world\"";
+            default:
+                return "new " + typeName + "()";
+        }
     }
 
     private static String truncateClass(String qualifiedName) {
@@ -96,6 +134,11 @@ public class JavaHint implements EditorHints.Hint {
 
         return qualifiedName.substring(lastPeriodIndex + 1);
     }
+
+    private final String PROBLEM_TEXT;
+    private final String SUGGESTION_TEXT;
+    private final List<String> GOOD_CODE;
+    private final List<String> BAD_CODE;
 
     public JavaHint(String problemText, String suggestionText) {
         PROBLEM_TEXT = problemText;
