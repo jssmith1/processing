54c44dce207e8b7e1536611342321d2a3dff6734
soir20 <71418127+soir20@users.noreply.github.com>
2021-07-06 17:46:51 -0400

    Remove unnecessary suppressions

--- java/src/processing/mode/java/pdex/MatchingRefURLAssembler.java
+++ java/src/processing/mode/java/pdex/MatchingRefURLAssembler.java
@@ -38,7 +38,6 @@ import java.util.stream.IntStream;
  * Creates URLs for MatchingRef errors based on the AST.
  * @author soir20
  */
-@SuppressWarnings({"SimplifyOptionalCallChains", "PatternVariableCanBeUsed"})
 public class MatchingRefURLAssembler {
     private static final String URL = "http://139.147.9.247/";
     private final String GLOBAL_PARAMS;
